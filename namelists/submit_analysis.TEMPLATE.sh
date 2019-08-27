bc79c7efa5edb3b25cc27e9c726191d63a7a2569
bsu-wrudisill <williamrudisill@u.boisestate.edu>
2019-08-27 13:20:11 -0600

    no more defq

--- namelists/submit_analysis.TEMPLATE.sh
+++ namelists/submit_analysis.TEMPLATE.sh
@@ -3,7 +3,7 @@
 #SBATCH -o analysis_wh.o%j         # Output and error file name (%j expands to jobID)
 #SBATCH -n 1                   # Total number of mpi tasks requested
 #SBATCH -N 1                   # Total number of nodes at 16 mpi tasks per-node requested
-#SBATCH -p shortq                   # Queue (partition) -- normal, development, etc.
+#SBATCH -p leaf                   # Queue (partition) -- normal, development, etc.
 #SBATCH -t 00:20:00                # Run time (hh:mm:ss) - 2.0 hours
 
 
