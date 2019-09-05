cc6d87f6be9003532c41db9f6b4e862963417a50
bsu-wrudisill <williamrudisill@u.boisestate.edu>
2019-09-05 14:52:55 -0600

    add read lat lon to the data retrieval

--- lib/R/fetchUSGSobs.R
+++ lib/R/fetchUSGSobs.R
@@ -14,15 +14,27 @@ library(dataRetrieval)
 # The discharge code is "00060" and when you pull the data is in cfs, so you need to change it to cms
 parameterCd <- "00060"  # Discharge
 obsDischarge <- dataRetrieval::readNWISuv(siteNumber, parameterCd, startDate, endDate)
+
+# read some extra data here 
+siteData <- dataRetrieval::readNWISsite(siteNumber)
 obsDischarge <- as.data.table(obsDischarge)
+
+# convert cfs to cms -- usgs data is in cfs, WRF-Hydro is in cms  
 cfsToCms <- 1/35.31466621266132
+
+# convert obs data to cms
 obsDischarge[, `:=`(discharge.cms=X_00060_00000*cfsToCms)]
 
 # add the day information to it 
 obsDischarge[, Date := format(dateTime, "%Y-%m-%d")]
 
-# average over each day, each USGS gages to get the daily flow 
+#!!!!!!!!!!!!!! AVERAGING DISCHARGE OVER EACH DAY ------- CHANGE ME LATER------- !!!!!!!!!!!!!!!!!!!#
+#                 average over each day, each USGS gages to get the daily flow 
+#
 obsDT <- obsDischarge[, .(obs = mean(discharge.cms, na.rm = TRUE)), by = c("Date", "agency_cd", "site_no")]
+#!!
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!# 
+
 
 # convert the date information from character to date format
 obsDT[, Date := as.Date(Date, tz = "UTC")]
@@ -32,6 +44,9 @@ obsStrData <- obsDT
 
 # Add in a POSIXct column, which is also expected by the workflow. 
 obsStrData$POSIXct <- as.POSIXct(obsStrData$Date,tz='UTC')
+obsStrData$lat <- siteData$dec_lat_va
+obsStrData$lon <- siteData$dec_long_va
+obsStrData$site_name_long <- siteData$station_nm
 
 # save it to a file
 write.csv(obsStrData, file = filePath)
