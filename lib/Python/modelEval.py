cd6d0511112b5d276fb37c7ddfc94384355c737c
bsu-wrudisill <williamrudisill@u.boisestate.edu>
2019-09-11 13:55:42 -0600

    remove unnecessary modle loads

--- lib/Python/modelEval.py
+++ lib/Python/modelEval.py
@@ -1,23 +1,7 @@
-#import json
-#import shutil
-#import os 
-#import glob 
-#import subprocess
-#import xarray as xr
-#import pandas as pd
-#import functools as ft
-#import netCDF4 as nc
-#import numpy as np
-#from pathlib import Path
-#import logging 
-
 import sys 
-
 libPath = '/scratch/wrudisill/WRF-HYDRO_CALIB/lib/Python'  #CHANGE ME TO SOMETHING BETTER !!!!! 
 sys.path.insert(0,libPath)
 import dblogger as dbl
-#import accessories as acc 
-
 
 if __name__ == "__main__":
 	clbdirc = sys.argv[1]
