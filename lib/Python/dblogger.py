bba50b56b36eb970217d902fef5656eb25a5b8bc
bsu-wrudisill <williamrudisill@u.boisestate.edu>
2019-10-20 10:38:12 -0600

    fixes #2

--- lib/Python/dblogger.py
+++ lib/Python/dblogger.py
@@ -50,6 +50,8 @@ def logModelout(clbdirc, iteration):
 	obsQ.rename(index=str, columns={"Date":"time", "obs":"qObs"}, inplace=True)
 	obsQ.set_index('time',inplace=True)
 	obsQ.index = pd.to_datetime(obsQ.index)
+	# find the length between the dates --- this could be different than
+	# the time index if there are missing dates in the observations 
 	idx = pd.date_range(obsQ.index[0], obsQ.index[-1])
 
 	# check if there are missing times from the observations ...
@@ -59,7 +61,7 @@ def logModelout(clbdirc, iteration):
 		logger.info(message)
 	# reindex and interpolate
 	obs = obsQ.reindex(idx)
-	obs_interpolate = obsQ.interpolate()
+	obs_interpolate = obs.interpolate()
 	obs_interpolate['time'] = idx
 	lat = obsQ['lat'].iloc[0]
 	lon = obsQ['lon'].iloc[0]
