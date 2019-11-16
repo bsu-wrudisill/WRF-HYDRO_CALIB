756d47cc1c7c3bcdf803e343c60e6852a56d3f5e
bsu-wrudisill <williamrudisill@u.boisestate.edu>
2019-11-16 09:30:00 -0700

    updates to modules on R2

--- env_nwm_r2.sh
+++ env_nwm_r2.sh
@@ -17,10 +17,10 @@ module purge
 # now load modules
 module load shared
 module load git/64/2.12.2
-module load slurm/17.11.8
-module load intel/compiler/64/2018/18.0.3
-module load intel/mpi/64/2018/3.222
-module load intel/mkl/64/2018/3.222
+module load slurm/17.11.12
+module load intel/compiler/64/2018/18.0.5
+module load intel/mpi/64/2018/4.274
+module load intel/mkl/64/2018/4.274
 module load hdf5_18/intel/1.8.18-mpi
 module load netcdf/intel/64/4.4.1
 module load udunits/intel/64/2.2.24
