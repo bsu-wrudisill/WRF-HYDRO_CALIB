db3b3c78f672a00bae37e600c3ced3f1d0fbca26
bsu-wrudisill <williamrudisill@u.boisestate.edu>
2020-07-03 17:27:28 -0600

    this was waaay behind. works with current version

--- runoneyear.py
+++ runoneyear.py
@@ -1,74 +1,74 @@
-import shutil
-import os
-import time
 import sys
 import datetime
 import logging
-import argparse
-
+import os
+import shutil
 libPathList = ['./lib/Python', './util']
 for libPath in libPathList:
-	sys.path.insert(0,libPath)
-from adjustParameters import *
-from adjustForcings import adjustForcings
+    sys.path.insert(0, libPath)
+from SetMeUp import SetMeUp
+from Calibration import Calibration
+from Validation import Validation
 from sanityPreCheck import RunPreCheck, RunCalibCheck, RunPreSubmitTest
+from dblogger import readSqlDischarge
+import accessories as acc
+import matplotlib.pyplot as plt
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--logname", default='logname', type=str, help="name of log file")
-
-args = parser.parse_args()
-
-
-# setup the log file --- this will get passed to all of the imported modules!!!
-
-
+# ----- log -----
 suffix = datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S")
-logfile= '{}_{}.log'.format(args.logname, suffix)
+logfile = 'runoneyear_{}.log'.format(suffix)
 
 file_handler = logging.FileHandler(filename=logfile)
 stdout_handler = logging.StreamHandler(sys.stdout)
 logging.basicConfig(level=logging.INFO,
-		    format='%(asctime)s %(name)15s %(levelname)-8s %(message)s',
-		    datefmt='%a, %d %b %Y %H:%M:%S',
-		    handlers=[file_handler, stdout_handler]
-		    )
+                    format='%(asctime)s %(name)15s %(levelname)-8s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    handlers=[file_handler, stdout_handler]
+                    )
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
 
 # -----  main ------
 setupfile = 'setup.yaml'
 calibrationfile = 'calib_params.tbl'
 
-# start the logging with some handy info
-logger.warning('RUN ONE YEAR ONLY')
-logger.info('starting {}. using {} setup parameters' .format(__name__, setupfile))
-logger.info('Logging to file: {}/{}'.format(os.getcwd(), logfile))
-
-# ---- run 'sanity checks' -----
-if not RunPreCheck(setupfile).run_all(): sys.exit()
-#if not RunCalibCheck(setupfile).run_all(): sys.exit()  -- dont check params, we are not calibrating
+#if not RunPreCheck(setupfile).run_all(): sys.exit()
+#if not RunCalibCheck(setupfile).run_all(): sys.exit()
 
 # create the setup instance
 setup = SetMeUp(setupfile)
-setup()   # gather forcing files, create directories, etc.
 
-# BE CAREFUL --- maybe change this later .... make it a namelist option
-if setup.adjust_forcings:
-	logging.warning("manualy adjusting precipitation forcings")
-	adjustForcings(setup)
-#
+# remove dir if it exists...
+#if setup.parent_directory.exists():
+#    shutil.rmtree(setup.parent_directory, ignore_errors=True)
 
-# check that setup() was successful
-if not RunPreSubmitTest(setupfile).run_all(): sys.exit()
-calib = CalibrationMaster(setupfile)
-calib.ForwardModel()  # run just one timeperiod
+# get the current directory 
+#cwd = os.getcwd()
 
+# Calibrate
+calib = Calibration(setupfile)
+#calib.PrepareCalibration()
+#calib.AdjustCalibTable()
 
-## clean up
-logger.info('----- Calibration Complete -----')
-logger.info('moving logfile {} to directory {}'.format(logfile, setup.clbdirc))
-logfile='logfile_2019-10-29_210310.log'
-shutil.move(logfile, setup.clbdirc +'/'+ logfile)  # move the log file to the directory
-# make some plots or something ....
+#calib.CreateAnalScript(calib.clbdirc, 'Calibration.db', 0) 
+final_file = calib.clbdirc.joinpath(calib.final_chrtfile)
 
+# Run the model once
+success, message = acc.ForwardModel(calib.clbdirc,
+                           calib.userid,
+                           calib.catchid,
+                           final_file)
+
+# submit the analysis...
+os.chdir(calib.clbdirc)
+jobid, err = acc.Submit('submit_analysis.sh', calib.catchid)
+
+# make some plots ...
+df = readSqlDischarge(calib.clbdirc.joinpath('Calibration.db'), 0)
+plt.plot(df.time, df.qMod, label=str(self.parent_directory.name))
+plt.plot(df.time, df.qObs, label='qobs')
+plt.legend()
+plt.savefig(calib.clbdirc.joinpath('qplot'))
